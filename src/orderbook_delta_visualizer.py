daed7075136d16d09909a2b53996d46f52b3276c
dineshpinto <dkpinto95@gmail.com>
2022-06-09 21:42:07 +0200

    Improved plotting in dash

--- src/orderbook_delta_visualizer.py
+++ src/orderbook_delta_visualizer.py
@@ -14,27 +14,29 @@ from plotly.subplots import make_subplots
 from ftx_websocket_client import FtxWebsocketClient
 
 
-def get_bid_ask_and_delta(market: str) -> Tuple[float, float, float]:
+def get_bid_ask_and_delta(market: str) -> Tuple[float, float, float, float, float]:
     orderbook = ftx.get_orderbook(market)
 
     bid_price, bid_volume = orderbook["bids"][0]
     ask_price, ask_volume = orderbook["asks"][0]
 
     delta = bid_volume - ask_volume
-    return bid_price, ask_price, delta
+    return bid_price, ask_price, bid_volume, ask_volume, delta
 
 
 def update_deque_lists():
     timestamps.append(datetime.datetime.now())
 
-    spot_bid_price, spot_ask_price, spot_delta = get_bid_ask_and_delta(SPOT_MARKET)
+    spot_bid_price, spot_ask_price, _, _, spot_delta = get_bid_ask_and_delta(SPOT_MARKET)
     spot_bids.append(spot_bid_price)
     spot_asks.append(spot_ask_price)
     spot_deltas.append(spot_delta)
 
-    perp_bid_price, perp_ask_price, perp_delta = get_bid_ask_and_delta(PERP_FUTURE)
+    perp_bid_price, perp_ask_price, perp_bid_volume, perp_ask_volume, perp_delta = get_bid_ask_and_delta(PERP_FUTURE)
     perp_bids.append(perp_bid_price)
     perp_asks.append(perp_ask_price)
+    perp_ask_volumes.append(perp_ask_volume)
+    perp_bid_volumes.append(perp_bid_volume)
     perp_deltas.append(perp_delta)
 
 
@@ -43,7 +45,7 @@ app = dash.Dash(__name__)
 app.layout = html.Div(
     [
         dcc.Graph(id='live-graph'),
-        dcc.Interval(id='graph-update', interval=2500, n_intervals=0),
+        dcc.Interval(id='graph-update', interval=1000, n_intervals=0),
     ]
 )
 
@@ -55,10 +57,10 @@ app.layout = html.Div(
 def update_graph_scatter(_):
     update_deque_lists()
 
-    fig = make_subplots(rows=3, cols=1, shared_xaxes=True,
+    fig = make_subplots(rows=4, cols=1, shared_xaxes=True,
                         subplot_titles=(
-                            f"{SPOT_MARKET}", f"{PERP_FUTURE}",
-                            "Delta"))
+                            f"{SPOT_MARKET} price", f"{PERP_FUTURE} price",
+                            f"{PERP_FUTURE} volume", f" Delta volume"))
 
     # Spot bids and asks
     fig.append_trace(
@@ -66,7 +68,7 @@ def update_graph_scatter(_):
             x=list(timestamps),
             y=list(spot_bids),
             name='Spot Bids',
-            mode='lines+markers'
+            mode='lines'
         ),
         row=1,
         col=1
@@ -76,7 +78,7 @@ def update_graph_scatter(_):
             x=list(timestamps),
             y=list(spot_asks),
             name='Spot Asks',
-            mode='lines+markers'
+            mode='lines'
         ),
         row=1,
         col=1
@@ -88,7 +90,7 @@ def update_graph_scatter(_):
             x=list(timestamps),
             y=list(perp_bids),
             name='Perp Bids',
-            mode='lines+markers'
+            mode='lines'
         ),
         row=2,
         col=1
@@ -98,9 +100,31 @@ def update_graph_scatter(_):
             x=list(timestamps),
             y=list(perp_asks),
             name='Perp Asks',
-            mode='lines+markers'
+            mode='lines'
         ),
         row=2,
+        col=1
+    )
+
+    # Volumes
+    fig.append_trace(
+        go.Scatter(
+            x=list(timestamps),
+            y=list(perp_ask_volumes),
+            name='Perp Ask Volume',
+            mode='lines+markers'
+        ),
+        row=3,
+        col=1
+    )
+    fig.append_trace(
+        go.Scatter(
+            x=list(timestamps),
+            y=list(perp_bid_volumes),
+            name='Perp Bid Volume',
+            mode='lines+markers'
+        ),
+        row=3,
         col=1
     )
 
@@ -116,7 +140,7 @@ def update_graph_scatter(_):
             name='Spot Delta',
             mode='lines+markers'
         ),
-        row=3,
+        row=4,
         col=1
     )
     fig.append_trace(
@@ -126,7 +150,7 @@ def update_graph_scatter(_):
             name='Perp Delta',
             mode='lines+markers'
         ),
-        row=3,
+        row=4,
         col=1
     )
 
@@ -140,7 +164,7 @@ def update_graph_scatter(_):
                 name='Lower BB',
                 mode='lines'
             ),
-            row=3,
+            row=4,
             col=1
         )
         fig.append_trace(
@@ -150,11 +174,12 @@ def update_graph_scatter(_):
                 name='Upper BB',
                 mode='lines'
             ),
-            row=3,
+            row=4,
             col=1
         )
 
     fig['layout'].update(
+        title_text=f"{SPOT_MARKET} and {PERP_FUTURE} bid-ask orderbook at depth=1",
         xaxis=dict(range=[min(timestamps), max(timestamps)]),
         width=1400,
         height=800,
@@ -165,7 +190,7 @@ def update_graph_scatter(_):
 
 if __name__ == '__main__':
     # Set up parameters
-    MAX_LENGTH = 50
+    MAX_LENGTH = 100
     pio.templates.default = "plotly_dark"
     SPOT_MARKET = "BTC/USD"
     PERP_FUTURE = "BTC-PERP"
@@ -182,6 +207,8 @@ if __name__ == '__main__':
     perp_asks = deque(maxlen=MAX_LENGTH)
     spot_deltas = deque(maxlen=MAX_LENGTH)
     perp_deltas = deque(maxlen=MAX_LENGTH)
+    perp_ask_volumes = deque(maxlen=MAX_LENGTH)
+    perp_bid_volumes = deque(maxlen=MAX_LENGTH)
 
     update_deque_lists()
 
