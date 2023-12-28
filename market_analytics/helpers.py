597988ffe7e71f09ca4642897c9fa3a18c034ba8
dineshpinto <annual.fallout_0z@icloud.com>
2023-12-29 00:59:26 +0530

    Restructure project

--- market_analytics/helpers.py
+++ market_analytics/helpers.py
@@ -1,8 +1,9 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 
-def calculate_annualized_volatility(df: pd.DataFrame, window: int, num_windows_in_year: int) -> pd.DataFrame:
+def calculate_annualized_volatility(df: pd.DataFrame, window: int,
+                                    num_windows_in_year: int) -> pd.DataFrame:
     """ Calculate volatility for a given dataframe and window size """
     annualization_factor = window / num_windows_in_year
 
